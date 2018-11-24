e13bb4f9420e8532df43b722b78283a4a21c6027
Kate Egle Sorotos <kate_sorotos@live.co.uk>
2018-11-24 11:08:55 +0000

    initial commit

--- js/engine.js
+++ js/engine.js
@@ -10,7 +10,7 @@ var play_you;
 var play_machine;
 
 /* Listen for the button clicks */
-$('button').click(function(event){
+$('.user-turn').click(function(event) {
   play_you = this.getAttribute("value");
   $('#play_you').text(play_you);
   console.log(play_you);
@@ -42,55 +42,73 @@ $('button').click(function(event){
 // });
 
 function computer_move() {
- var random_number = Math.floor(Math.random() * (3 - 0)) + 0;
+  var random_number = Math.floor(Math.random() * (3 - 0)) + 0;
 
- play_machine = choices[random_number];
- $("#play_machine").text(play_machine);
- console.log('move: ' + play_machine);
+  play_machine = choices[random_number];
+  $("#play_machine").text(play_machine);
+  console.log('move: ' + play_machine);
 }
 
-function compare(you, machine){
- var verdict = 'lose';
- if ( you == machine ) {
-   verdict = 'draw';
- }
- if (( you == "rock") && (machine == "scissors")) {
-   verdict = 'win';
- }
- if (( you == "paper") && (machine == "rock")) {
-   verdict = 'win';
- }
- if (( you == "scissors") && (machine == "paper")) {
- verdict = 'win';
- }
+function compare(you, machine) {
+  var verdict = 'lose';
+  if (you == machine) {
+    verdict = 'draw';
+  }
+  if ((you == "rock") && (machine == "scissors")) {
+    verdict = 'win';
+  }
+  if ((you == "paper") && (machine == "rock")) {
+    verdict = 'win';
+  }
+  if ((you == "scissors") && (machine == "paper")) {
+    verdict = 'win';
+  }
 
- if (( you == "paper") && (machine == "scissors")) {
-   verdict = 'lose';
- }
- if (( you == "scissors") && (machine == "rock")) {
-   verdict = 'lose';
- }
- if (( you == "rock") && (machine == "paper")) {
- verdict = 'lose';
- }
+  if ((you == "paper") && (machine == "scissors")) {
+    verdict = 'lose';
+  }
+  if ((you == "scissors") && (machine == "rock")) {
+    verdict = 'lose';
+  }
+  if ((you == "rock") && (machine == "paper")) {
+    verdict = 'lose';
+  }
 
- switch (verdict) {
-  case 'win':
-  won++;
-   $("#game_won").text(won);
-   break;
-  case 'lose':
-   lost++;
-   $("#game_lost").text(lost);
-   break;
-  case 'draw':
-   draw++;
-  $("#game_draw").text(draw);
-  break;
+  switch (verdict) {
+    case 'win':
+      won++;
+      $("#game_won").text(won);
+      break;
+    case 'lose':
+      lost++;
+      $("#game_lost").text(lost);
+      break;
+    case 'draw':
+      draw++;
+      $("#game_draw").text(draw);
+      break;
+  }
+
+  $("#verdict").text(verdict);
+
+  played++;
+  $("#game_played").text(played);
 }
 
- $("#verdict").text(verdict);
+$('#reset').click(function(event) {
+      played = 0;
+      won = 0;
+      lost = 0;
+      draw = 0;
+      play_you;
+      play_machine;
 
- played++;
- $("#game_played").text(played);
-}
+      $('#play_you').empty();
+      $("#play_machine").empty();
+      $("#game_played").empty();
+      $("#verdict").empty();
+      $("#game_won").empty();
+      $("#game_lost").empty();
+      $("#game_draw").empty();
+
+    })
